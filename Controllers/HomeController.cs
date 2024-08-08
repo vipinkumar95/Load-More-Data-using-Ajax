264879d5fe801ac5151c7276fd21cc8d2d0fe5f2
Rudra Pal <Rudra Pal@DESKTOP-EPCRK2D>
2024-08-08 12:39:11 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using LoadMoreData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoadMoreData.Controllers
+{
+    public class HomeController : Controller
+    {
+        LoadMoreEntities db = new LoadMoreEntities();
+
+        public ActionResult Index()
+        {
+            int num = 3;
+            Session["data"] = num;
+            var data = db.loadings.ToList().Take(num);
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Index(loading l)
+        {
+            int rows = Convert.ToInt32(Session["data"]) + 3;
+            var data = db.loadings.ToList().Take(rows);
+            Session["data"] = rows;
+            return PartialView("_Empdata", data); 
+        }
+        
+    }
+}
